dc78942dac51dd5a990c5231c92899a4a40dcf90
kirab-21 <86204980+kirab-21@users.noreply.github.com>
2023-08-16 01:30:54 +0530

    Completed Canvas

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -14,12 +14,12 @@ const CameraRig = ({ children }) => {
     const isMobile = window.innerWidth <=600;
 
     //set the initial position of the model
-    let targetPosition = [-0.4, 0.2];
+    let targetPosition = [-0.4, 0, 2];
     if(snap.intro) {
       if(isBreakpoint) targetPosition = [0, 0, 2];
       if(isMobile) targetPosition = [0,0.2,2.5];
     }else{
-      if(isMobile) targetPosition = [0,0.2,2.5];
+      if(isMobile) targetPosition = [0,0,2.5];
       else targetPosition = [0,0,2]
     }
 
@@ -30,14 +30,14 @@ const CameraRig = ({ children }) => {
     //set the model rotation
     easing.dampE(
       group.current.rotation,
-      [state.pointer.y/10, -state.pointer.x/5,0],
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
       0.25,
       delta
     )
   })
 
  
-  return <group ref = {group}>{children}</group>
+  return <group ref={group}>{children}</group>
 }
 
 export default CameraRig
