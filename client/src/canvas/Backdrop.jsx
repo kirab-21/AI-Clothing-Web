c553588288c0ee6acce09975c991c23a81b288bf
kirab-21 <86204980+kirab-21@users.noreply.github.com>
2023-08-22 21:00:06 +0530

    minor changes

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -4,7 +4,7 @@ import { useFrame } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 
 
-export const Backdrop = () => {
+const Backdrop = () => {
 
   const shadows = useRef()
 
