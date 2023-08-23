ea06f2a8ad52af31bb9bbb4988c6960f4d3856cb
kirab-21 <86204980+kirab-21@users.noreply.github.com>
2023-08-23 12:24:57 +0530

    fixing error

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,13 +1,43 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import * as ai from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
+//const config = new Configuration({
+  //  apiKey: process.env.OPENAI_API_KEY,
+//})
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const openai = new OpenAI(OPENAI_API_KEY)
+// const openai = new OpenAI({
+//     apiKey: process.env.OPENAI_API_KEY ,// This is also the default, can be omitted
+// });
+
 router.route('/').get((req, res) => {
     res.status(200).json({message: "Hi from dalle routes"})
+})
+
+router.route('/').post(async(req, res)=> {
+    try {
+        const { prompt } =req.body;
+
+        const response = await openai.createImage({
+            prompt,
+            n:1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        });
+
+        const image = response.data.data[0].b64_json;
+
+        res.status(200).json({photo:image })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Something went Wrong'})
+    }
 })
 
 export default router;
